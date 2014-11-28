9ae2559e10627147a43da2d54b4c8b59977d47b2
Achie Liang <shaochiliang@gmail.com>
2014-11-28 16:41:42 +0800

    1. Add CFAvatarView 2. Make app full screen 3. Add SwipeView pod

---
+++ ios/coffee-oc/coffee/coffee/UIKit/CFAvatarView.h
@@ -0,0 +1,38 @@
+//
+//  CFAvatarView.h
+//  coffee
+//
+//  Created by achiel on 14/11/26.
+//  Copyright (c) 2014年 1clip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFAvatarView : UIView
+
+@property (nonatomic, strong) UIColor *borderColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) CGFloat borderWidth UI_APPEARANCE_SELECTOR;
+
+
+- (void)setImageWithURL:(NSURL *)url;
+
+- (void)setImageWithURL:(NSURL *)url
+       placeholderImage:(UIImage *)placeholderImage;
+
+- (void)setImageWithURL:(NSURL *)url
+        placeholderText:(NSString *)placeholderText
+     placeholderBgColor:(UIColor *)placeholderBgColor;
+
+- (void)setImageWithURL:(NSURL *)url
+       placeholderImage:(UIImage *)placeholderImage
+             badgeImage:(UIImage *)badgeImage;
+
+- (void)setImageWithURL:(NSURL *)url
+        placeholderText:(NSString *)placeholderText
+     placeholderBgColor:(UIColor *)placeholderBgColor
+             badgeImage:(UIImage *)badgeImage;
+
+
+
+
+@end
